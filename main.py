4bb2d8d1ad919a7ea48d4881e7daf104388bfb10
Artur Sanin <artur.sanin@web.de>
2024-06-02 19:57:03 +0200

    Defined df_report function.

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 """
-    Begin of first project.
+    Project description:
 """
+import pandas as pd
+
+
+def df_report(file_path):
+    """
+
+    :param file_path: File Path to the csv file of the data.
+    :return: Creates a full report on the Data.
+    """
+    # Getting the name of the file, by separating the file_path at / and getting the last string from the list
+    # created by the methode split.
+    split_list = file_path.split("/")
+    file_name = split_list[len(split_list) - 1]
+    print("This is a Report on the Dataset" + "\033[1m", file_name + ".", "\033[0m")
